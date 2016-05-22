5e854529b87d2d241630907ec880ce2962f351a0
JiangQu <jqonline@gmail.com>
2016-05-22 21:19:11 +0800

    Parse command line

--- main.py
+++ main.py
@@ -1,1 +1,18 @@
 __author__ = 'jqol'
+
+import argparse
+from core import InstaCrawler
+
+parser = argparse.ArgumentParser()
+parser.add_argument('username')
+parser.add_argument(
+    '-o',
+    '--output',
+    help='Specify output directory, cd/download is the default output dir')
+args = parser.parse_args()
+
+crawler = InstaCrawler(args.username)
+crawler.refresh_list()
+
+
+
